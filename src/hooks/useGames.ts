ac6e166cffbc763f3dff93a7010254c42cdbcbd1
Victorpraiz <ivictorpraiz@gmail.com>
2023-03-16 19:53:16 +0100

    added badges scores

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,7 +13,8 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[]
+  parent_platforms: {platform: Platform}[];
+  metacritic: number;
 }
 
 
